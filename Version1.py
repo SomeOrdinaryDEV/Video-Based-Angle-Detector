4d9397450b28f604bb750112d0075b84f022fb54
SomeOrdinaryDEV <97533743+SomeOrdinaryDEV@users.noreply.github.com>
2023-10-06 15:26:18 +0530

    Update Version1.py
    as angletest.py

--- Version1.py
+++ Version1.py
@@ -2,25 +2,42 @@ import numpy as np
 from skimage.transform import (hough_line, hough_line_peaks)
 import cv2
 from matplotlib import pyplot as plt
-import time
+# reflection problems on computer vision
+# distortion problems (non visible solid edges)
+# use opencv vision tracker 
+
 
 cap = cv2.VideoCapture('Video Data/Vid1.mov')
 counter = 1
-frame_second = 0
 angle_addition = 0 
-cur_angle = 0
-anglelist = []
-mean = 0
+str_angle = ""
+#use cartesian reference frame
+#point clusters(hough lines, hough peaks) 
 
-def angles(edges):
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    if ret == False:
+       cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+       continue
+    # Our operations on the frame come here
+    edges = cv2.Canny(cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), (11,11), 0), 100, 170)
+    #gray = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
+    lines = cv2.HoughLinesP(edges, 35, np.pi/180, 50, maxLineGap=10)
+    
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
     tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)
     hspace, theta, dist = hough_line(edges, tested_angles)
     h, q, d = hough_line_peaks(hspace, theta, dist)
+    
+
     angle_list = []
-        
+    
     fig, axes = plt.subplots(1, 3, figsize=(15, 6))
     ax = axes.ravel()
-
     
     origin = np.array((0, edges.shape[1]))
 
@@ -31,38 +48,30 @@ def angles(edges):
     angles = [a*180/np.pi for a in angle_list]
 
     # Compute difference between the two lines
-    angle_difference = np.max(angles) - np.min(angles)
-    return angle_difference
-
-#use cartesian reference frame
-#point clusters(hough lines, hough peaks) 
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    angle_difference = np.max(angles) - np.min(angles)  
+    if counter % 30 != 0:
+        angle_addition = angle_addition + angle
+    else:
+        angle_addition = angle_addition/30
     
-    # Our operations on the frame come here
-    edges = cv2.Canny(cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), (11,11), 0), 100, 170)    
-    
-    if counter % 3 == 0:
-        angle_difference = angles(edges)
-        angle_addition = angle_addition + angle_difference
-        if counter%30 == 0:
-            anglelist.append((round(angle_addition/10, 2)))
-            print(anglelist[frame_second])
-            angle_addition = 0
-            frame_second = frame_second + 1
+    if round(180 - angle_difference, 3) >= round(angle_difference, 3) and angle_difference >= 90:
+        print(round(180 - angle_difference, 3))  #Subtracting from 180 to show it as the small angle between two lines
+        str_angle = str (round(180- angle_difference), 3)
+        cv2.putText(frame, str_angle, (150,200), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+    else:
+        print(round(angle_difference, 3))
+        str_angle = str (round(angle_difference, 3))
+        cv2.putText(frame, str_angle, (150,200), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+    plt.close('all')
         
-         
+
+    counter = counter + 1
+    cv2.imshow('edges after hough lines', edges)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    plt.close('all')
-    counter = counter + 1
-anglelist.sort()
-print('end of angles, start of list')
+    cv2.imshow("frame", frame)
 
-for i in range(len(anglelist)):
-    print(anglelist[i])
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
